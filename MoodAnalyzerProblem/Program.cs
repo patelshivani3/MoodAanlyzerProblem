dc044aca721e00d58591ff3a135ad1744d365531
patelshivani3 <shivpatel3001@gmail.com>
2023-01-17 09:01:33 +0530

    Adding Welcome to Mood Analyzer Program

---
+++ MoodAnalyzerProblem/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MoodAnalyzerProblem
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("\nWelcome to Mood Analyzer Program");
+            Console.ReadLine();
+        }
+    }
+}
